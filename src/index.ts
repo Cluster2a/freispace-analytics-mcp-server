06624f502082c941b19351f2cb6380dbaa788d8f
Alexander Hirschfeld <alexander@hirsch-feld.de>
2025-08-04 00:43:57 +0200

    chore: update version to 1.0.9 in package.json and index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { setupJsonConsole } from "./utils/console.js";
 
 setupJsonConsole();
 
-const VERSION = "1.0.8";
+const VERSION = "1.0.9";
 const server = new McpServer({
   name: "freispace",
   version: VERSION,
